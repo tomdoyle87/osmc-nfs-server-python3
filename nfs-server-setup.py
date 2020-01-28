b5d8f65f0567598318f71f4b72200562395edcbe
tomdoyle87 <36793507+tomdoyle87@users.noreply.github.com>
2020-01-28 20:47:21 +0000

    Yes/no inputs moved to validation function.

--- nfs-server-setup.py
+++ nfs-server-setup.py
@@ -24,37 +24,44 @@ def check(Ip):
         print("Invalid Ip address, please try again")
         ip ='0'
 
-REPLY = ' '
-while REPLY != 'yes' or REPLY != 'no':
-    REPLY = input('Do you want to setup an NFS server?  (yes/no?): ')
-    if REPLY == 'yes':
-        print("Installing Server")
-        break
-    if REPLY == 'no':
-        print("Exiting Setup")
-        sys.exit()
+def yes_or_no(question):
+    global reply
+    answer = input(question + "(yes/no): ").lower().strip()
+    print("")
+    while not(answer == "y" or answer == "yes" or \
+    answer == "n" or answer == "no"):
+        print("Input yes or no")
+        answer = input(question + "(yes/no):").lower().strip()
+        print("")
+    if answer[0] == "y":
+        return True
+        reply = '1'
+    if answer[0] == "n":
+        reply = '0'
     else:
-        print("Invalid input, please try again?")
+        return False
+
+reply = ' '
+question = yes_or_no('Do you want to setup an NFS server? ')
+if reply == '0':
+   print("Exiting Setup")
+   sys.exit()
 
 ip = ' '
-REPLY = ' '
-while REPLY != 'yes' or REPLY != 'no':
-    REPLY = input('Do you want Restrict what IPs can access the Server? (yes/no?): ')
-    #print(REPLY)
-    if REPLY != 'yes' or REPLY != 'no':
-        print("Invalid input, please try again?")        
-        if REPLY == 'yes':
-            while ip !='1':
-                Ip = ' '
-                Ip = input ('Please enter an IP network, for example 192.168.0.1/24. Or A single host, e.g. 192.168.1.15 ')
-                check(Ip)
-                if ip == '1':
-                   continue
-                if ip == '0':
-                   continue  
-            break
-        if REPLY == 'no':
-            print("Not restricting IP")
-            Ip = '?'
-            break
+print("Installing Server")
+reply = ' '
+question = yes_or_no('Do you want Restrict what IPs can access the Server? ')
+if reply != '0':
+    while ip !='1':
+         Ip = input ('Please enter an IP network, for example 192.168.0.1/24. Or A single host, e.g. 192.168.1.15 ')
+         check(Ip)
+         if ip == '1':
+             break
+         if ip == '0':
+             continue
+
+if reply == '0':
+    print("Not restricting IP")
+    Ip = '?'
+
 print(Ip)
